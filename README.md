a59facaf318a54a83100e83cd30c6c7f967cceee
sagi <sagipael@gmail.com>
2025-07-10 20:02:39 +0300

    - update readme - change variables names - add variable for CRON_TIME

--- README.md
+++ README.md
@@ -6,20 +6,28 @@ Cloudflare tunnel + DDNS + LetsEncrypt cert generator
 
 | Parameter | Function | Comment |
 | :----: | --- | --- |
-| `TUNNEL_TOKEN` | Cloudflare Tunnel token | Follow instructions below |
-| `TOKEN` | Cloudflare account token, with edit permissions for DNS | [API Token](https://dash.cloudflare.com/profile/api-tokens) |
-| `A_RECORD` | A record for Dynamic DNS | example: dynamic|
-| `DOMAIN` | Your managed Domain name in cloudflare | example: example.com |
+| `CF_TUNNEL_TOKEN` | Cloudflare Tunnel token | Follow instructions below |
+| `CF_DNS_TOKEN` | Cloudflare account token, with **edit** permissions for DNS | [API Token](https://dash.cloudflare.com/profile/api-tokens) |
+| `CF_A_RECORD` | A record for Dynamic DNS | example: dynamic|
+| `CF_DOMAIN` | Your managed Domain name in cloudflare | example: example.com |
 | `SCHEDULE` | set time for recurrent ddns update  | examples: 1h/1d/2mo|
 | `SSL` | Enable/disable wildcard certificate generator | true/false |
 
 
 
 ## To get the Cloudflare Tunnel token, follow these steps:
-
-Log in to your [Cloudflare Dashboard](https://one.dash.cloudflare.com/).
-Navigate to the Zero Trust
-Select Tunnels from the navigation menu.
-Click on Create a Tunnel.
-Follow the on-screen instructions to name your tunnel and select your desired configuration.
+(CF_TUNNEL_TOKEN)<br>
+Log in to your [Cloudflare Dashboard](https://one.dash.cloudflare.com/).<br>
+Navigate to the Zero Trust<br>
+Select Tunnels from the navigation menu.<br>
+Click on Create a Tunnel.<br>
+Follow the on-screen instructions to name your tunnel and select your desired configuration.<br>
 Once the tunnel is created, Cloudflare will provide a Tunnel Token. Copy this token and paste it into the .env file under TUNNEL_TOKEN.
+
+
+## Notes
+* CF_A_RECORD must be created in advance
+* certbot runs every day at 3AM (CRON_TIME='0 3 * * *' )<br>
+to change it, set cron pattern as CRON_TIME
+
+
--- entrypoint.sh
+++ entrypoint.sh
@@ -10,12 +10,12 @@ function echoLog {
 }
 
 function get_record_id {
-	echo "Resolving record ID for $A_RECORD"
-	A_RECORD_ID=$(curl -s "${API_BASE}/zones/${ZONE_ID}/dns_records?type=A&name=${A_RECORD}.${DOMAIN}" "${HEADERS[@]}" | jq -r '.result[0].id')
+	echo "Resolving record ID for $CF_A_RECORD"
+	A_RECORD_ID=$(curl -s "${API_BASE}/zones/${ZONE_ID}/dns_records?type=A&name=${CF_A_RECORD}.${CF_DOMAIN}" "${HEADERS[@]}" | jq -r '.result[0].id')
 
 	if [[ -z "$A_RECORD_ID" || "$A_RECORD_ID" == "null" ]]; then
-		echoLog	"[ERROR]	Unable to resolve A record ID for: ${A_RECORD}.${DOMAIN}"
-		curl "${API_BASE}/zones/${ZONE_ID}/dns_records?type=A&name=${A_RECORD}.${DOMAIN}" "${HEADERS[@]}"
+		echoLog	"[ERROR]	Unable to resolve A record ID for: ${CF_A_RECORD}.${CF_DOMAIN}"
+		curl "${API_BASE}/zones/${ZONE_ID}/dns_records?type=A&name=${CF_A_RECORD}.${CF_DOMAIN}" "${HEADERS[@]}"
 		exit 1
 	fi
 
@@ -25,21 +25,29 @@ function get_record_id {
 }
 
 function get_zone_id {
-	echo "Resolving zone ID for: $DOMAIN"
-	RES="$(curl -s -X GET "${API_BASE}/zones?name=$DOMAIN" "${HEADERS[@]}")"
+	echo "Resolving zone ID for: $CF_DOMAIN"
+	RES="$(curl -s -X GET "${API_BASE}/zones?name=$CF_DOMAIN" "${HEADERS[@]}")"
 	ZONE_ID="$(echo "$RES" | jq -r '.result[0].id')"
 
 	if [[ -z "$ZONE_ID" || "$ZONE_ID" == "null" ]]; then
-		echoLog	"[ERROR]	Unable to resolve Zone ID for: ${DOMAIN}"
-		curl  -X GET "${API_BASE}/zones?name=$DOMAIN" "${HEADERS[@]}"
+		echoLog	"[ERROR]	Unable to resolve Zone ID for: ${CF_DOMAIN}"
+		curl  -X GET "${API_BASE}/zones?name=$CF_DOMAIN" "${HEADERS[@]}"
 		exit 1
 	fi
-	MAIL="$(echo "$RES" | jq -r '.result[0].account.name' | grep -i -o -E "[a-z0-9\.-]+@[a-z0-9\.-]+")"
-	MAIL=${MAIL,,}
-
+	
+	# get mail if not set in env
+	if [[ ! "$MAIL" ]] ; then
+		MAIL="$(echo "$RES" | jq -r '.result[0].account.name' | grep -i -o -E "[a-z0-9\.-]+@[a-z0-9\.-]+")"
+		MAIL=${MAIL,,}
+		export MAIL
+	fi
+	
+	if [[ "${SSL,,}" == "true" && -z "$MAIL" ]]; then
+		echoLog	"[ERROR]	If SSL is set, MAIL cannot be empty."
+		exit 1
+	fi
+		
 	export ZONE_ID
-	export MAIL
-
 	# echo "ZONE_ID: $ZONE_ID"
 }
 
@@ -51,7 +59,7 @@ function update_ip {
 		echoLog "[INFO]	IP changed: $OLD_IP → $CURRENT_IP"
 		RECORD=$(cat <<- EOF
 			{ "type": "A",
-			"name": "$A_RECORD",
+			"name": "$CF_A_RECORD",
 			"comment": "updated by script at $(date +%F_%T)",
 			"content": "$CURRENT_IP",
 			"ttl": 180,
@@ -94,20 +102,20 @@ function parse_schedule {
 DATA=/data
 TMP_IP_FILE="$DATA/current_ip"
 RECORD_ID_FILE="$DATA/record_id"
-
 API_BASE="https://api.cloudflare.com/client/v4"
-
+[[ ! "$CRON_TIME" ]] && { CRON_TIME="0 3 * * *" ; export CRON_TIME ; }
+ 
 mkdir -p $DATA
 
 
-[[ ! "$TOKEN" ]] && { echoLog "[ERROR]	TOKEN cannot be empty" ; ERROR=1 ; }
-[[ ! "$A_RECORD" ]] && { echoLog "[ERROR]	A_RECORD cannot be empty" ; ERROR=1 ; }
-[[ ! "$DOMAIN" ]] && { echoLog "[ERROR]	DOMAIN cannot be empty" ; ERROR=1 ; }
+[[ ! "$CF_DNS_TOKEN" ]] && { echoLog "[ERROR]	CF_DNS_TOKEN cannot be empty" ; ERROR=1 ; }
+[[ ! "$CF_A_RECORD" ]] && { echoLog "[ERROR]	CF_A_RECORD cannot be empty" ; ERROR=1 ; }
+[[ ! "$CF_DOMAIN" ]] && { echoLog "[ERROR]	CF_DOMAIN cannot be empty" ; ERROR=1 ; }
 
 
 [[ "$ERROR" ]] && exit 1
 HEADERS=(
-  -H "Authorization: Bearer $TOKEN"
+  -H "Authorization: Bearer $CF_DNS_TOKEN"
   -H "Content-Type: application/json"
 )
 
@@ -124,16 +132,16 @@ get_record_id
 
 
 
-if [ -z "$A_RECORD_ID" ] || [ "$A_RECORD_ID" = "null" ]; then
-	echoLog "[ERROR]	Unable to resolve A record ID for $A_RECORD"
+if [ -z "$CF_A_RECORD" ] || [ "$CF_A_RECORD" = "null" ]; then
+	echoLog "[ERROR]	Unable to resolve A record ID for $CF_A_RECORD"
 	exit 1
 fi
 
 echo -e "\n------------------------------------------------------------------"
 echoLog "[DEBUG]	MAIL:		$MAIL"
 echoLog "[DEBUG]	ZONE_ID:	$ZONE_ID"
-echoLog "[DEBUG]	A_RECORD_ID:	$A_RECORD_ID"
-echoLog "[DEBUG]	A Record:	${A_RECORD}.$DOMAIN"
+echoLog "[DEBUG]	CF_A_RECORD:	$CF_A_RECORD"
+echoLog "[DEBUG]	A Record:	${CF_A_RECORD}.$CF_DOMAIN"
 echo -e "------------------------------------------------------------------\n"
 
 
@@ -144,14 +152,14 @@ if [[ "${SSL,,}" == "true" ]] ; then
 	sleep 1
 	CERTBOT_CONFIG=/etc/letsencrypt/cloudflare.ini
 	mkdir -p $(dirname $CERTBOT_CONFIG)
-	echo "dns_cloudflare_api_token = $TOKEN" > $CERTBOT_CONFIG
+	echo "dns_cloudflare_api_token = $CF_DNS_TOKEN" > $CERTBOT_CONFIG
 	chmod 600 $CERTBOT_CONFIG
 
-	bash -x -c "certbot certonly --agree-tos -n -m $MAIL --dns-cloudflare --dns-cloudflare-credentials $CERTBOT_CONFIG -d \"*.$DOMAIN\" -d \"$DOMAIN\" || true"
+	bash -x -c "certbot certonly --agree-tos -n -m $MAIL --dns-cloudflare --dns-cloudflare-credentials $CERTBOT_CONFIG -d \"*.$CF_DOMAIN\" -d \"$CF_DOMAIN\" || true"
 
 	echo "[DEBUG]	Certificate Info:"
 	certbot certificates
-	CRON_JOB="0 3 * * * certbot renew > $DATA/cert.lastrun 2>&1"
+	CRON_JOB="${CRON_TIME} certbot renew > $DATA/cert.lastrun 2>&1"
 	
 	echo "[DEBUG]	CRON line: ${CRON_JOB}"
 	
